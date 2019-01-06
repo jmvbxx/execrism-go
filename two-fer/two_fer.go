02b0e34bf71aeafa601f6d12ff30b90cf20c58ec
Jason Bornhoft <jbornhoft@doximity.com>
2019-01-06 12:14:14 -0500

    Updated response to remove else portion

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 )
 
-// ShareWith should have a comment documenting it. (Why is this? Lint? Norm?)
+// ShareWith takes in name string and returns sentence
 func ShareWith(name string) string {
-
 	if name == "" {
 		return "One for you, one for me."
-	} else {
-		return fmt.Sprintf("One for %s, one for me.", name)
 	}
-	// return ""
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
